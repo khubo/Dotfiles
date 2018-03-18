9bff2f32a507873ec2111d204a3931cff7093f4a
khubo(aswin) <aswinrajv@gmail.com>
2018-03-18 11:59:40 +0530

    Added npm global modules to path

--- fish/config.fish
+++ fish/config.fish
@@ -3,6 +3,11 @@ set -q XDG_DATA_HOME
   and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
   or set -gx OMF_PATH "$HOME/.local/share/omf"
 
+# Add go path
+set -U fish_user_paths /usr/local/go/bin $fish_user_paths
+set -U fish_user_paths ~/.npm/bin $fish_user_paths
+set -x GOPATH /home/khubo/go
+
 # Load Oh My Fish configuration.
 source $OMF_PATH/init.fish
 
